35851e3e107dc828bdd54964920ad8d973420c04
Sajith <sajithsuresh2007@gmail.com>
2026-03-17 13:20:07 +0530

    feature/UC2-PrintHello Done

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,6 +1,5 @@
 public class HelloApp {
     public static void main(String[] args) {
-        String name = args[0];
-        System.out.println("Hello, " + name + "!");
+        System.out.println("Hello, World!");
     }
 }
